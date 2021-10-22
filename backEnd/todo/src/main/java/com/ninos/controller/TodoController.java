20336b1214f5fb8d9590420f5aaa4f43b765dabd
NinoosMoshi <nalkaton@gmail.com>
2021-10-22 10:04:29 -0400

    Spring TodoController(getall & getById) Closes #3

--- backEnd/todo/src/main/java/com/ninos/controller/TodoController.java
+++ backEnd/todo/src/main/java/com/ninos/controller/TodoController.java
@@ -3,10 +3,10 @@ package com.ninos.controller;
 import com.ninos.model.Todo;
 import com.ninos.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -25,8 +25,16 @@ public class TodoController {
         return todoRepository.save(todo);
     }
 
+   @GetMapping("/todo")
+   public List<Todo> getAllTodo(){
+        return todoRepository.findAll();
+   }
 
-
+   @GetMapping("/todo/{todoId}")
+   public Optional getTodo(@PathVariable("todoId") Long todoId){
+      var todo = todoRepository.findById(todoId);
+      return todo;
+   }
 
 
 
